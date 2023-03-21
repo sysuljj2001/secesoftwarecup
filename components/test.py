bde162aa3a08a1e1192152d567dd75ca576a8328
ljj <L496181303@gmail.com>
2023-03-22 01:47:25 +0800

    路径+pid

--- components/test.py
+++ components/test.py
@@ -42,17 +42,18 @@ class Robot:
         angle_to_goal = math.atan2(dy, dx)
         return angle_to_goal - self.heading
 
-    """
     def addpoint(self,point1,point2):
         self.path.append(point1)
         self.path.append(point2)
+
     def call(self):
         return self.current_path_index
-    """
+
     def update(self, dt):
         distance_to_goal = self.distance_to_goal()
         angle_to_goal = self.angle_to_goal()
-        if distance_to_goal < 0.5:
+
+        if distance_to_goal < 1:
             self.current_path_index += 1
             if self.current_path_index > len(self.path):
                 return False
@@ -69,49 +70,31 @@ class Robot:
         self.heading += angular_velocity * dt
         self.x += math.cos(self.heading) * linear_velocity * dt
         self.y += math.sin(self.heading) * linear_velocity * dt
-        #print(linear_velocity)
-        #print(angular_velocity)
-        return linear_velocity,angular_velocity
-
-class PID_Controller():
-    def __init__(self, bot_info: dict, paths: List, kp, ki, kd) -> None:
-        '''PID 控制器，输入一帧机器人位姿状态和当前帧行走路径集合，输出期望的线速度和角速度
-        :param bot_info: 从地图数据中获取的机器人位姿
-        :param paths: 机器人行走路径集合（有序点集）
-        '''
-        self.bot_info = bot_info
-        self.paths = paths
-        #self.pid = PidController(
-        self.robot = Robot(self.bot_info['x'],self.bot_info['y'],self.bot_info['p'],self.bots['tv'],self.bot_info['w'],self.paths)
-        #调用robot类，经由pid控制，通过update()函数得到控制指令
-    def update_paths(self, paths):
-        '''更新移动路径
-        '''
-        self.paths = paths
-
-    def update_bot(self, bot_info: dict):
-        '''更新机器人位姿状态
-        '''
-        self.bot_info = bot_info
-
-    def handle(self):
-        '''获取该机器人前往 dest 在当前帧所需的 forwad 和 rotate
-        '''
-        forwad,rotate=robot.update()
-        return forwad,rotate*pi/3
-
+        print(linear_velocity)
+        print(angular_velocity)
+        return True
+    
 if __name__ == '__main__':
-    #robot = Robot(x=10, y=7, heading=0, linear_speed=0, angular_speed=0, path=path)
+    path = [(45.5, 10.5),(31.5, 23.5)]
+    path1 = [(31.5, 10.5), (31.5, 26.5), (31.5, 23.5), (31.5, 10.5), (27.5, 20.5),(31.5, 26.5),(35.5, 10.5),(31.5, 23.5)]
+    robot = Robot(x=10, y=7, heading=0, linear_speed=0, angular_speed=0, path=path)
+
     x_, y_ = [], []
 
     while robot.update(dt=0.1):
         #print(f"x: {robot.x}, y: {robot.y}, heading: {robot.heading}, current_path: {robot.current_path_index}")
         #time.sleep(0.01)
+
         x_.append(robot.x)
         y_.append(robot.y)
         num=robot.call()
-        """
         if num % 2 == 0 and num != 0:
             robot.path.append(path1[num - 2])#动态更新
             robot.path.append(path1[num - 1])
-        """
+
+        plt.cla()
+        plt.scatter(np.array(path)[:, 0], np.array(path)[:, 1])
+        plt.plot(x_, y_, '-r', label = 'trajectory')
+        plt.grid()
+        plt.pause(0.001)
+    plt.show()
--- main.py
+++ main.py
@@ -2,8 +2,8 @@
 import sys
 from components import A_star, scheduler
 from components.preprocess import DataLoader
-from components.controller import PID_Controller
-
+from components.test import PID_Controller
+from components.test import Robot
 
 def read_util_ok():
     while input() != "OK":
@@ -28,13 +28,19 @@ if __name__ == '__main__':
         
         # 决策、控制
         bot_infos = dataloader.bots
-        
         # 2023/03/20 feature: 完成一个机器人的 PID 控制
-
-        pid_controller = PID_Controller(bot_info=bot_infos[0], paths=[]) # 随便去一个地方
+        #for i in range(4)作各机器人判断
+        needpath = A_find([bot_infos[0]['x'],bot_infos[0]['y']],[0,0])#假设点0,0，得出离散路径
+        pid_controller = PID_Controller(bot_info=bot_infos[0], paths=needpath) #调用机器人控制类
+        c1_1,c1_2 = pid_controller.handle()#当前帧所需控制指令
+        print('0')
+        print(f'forward {i} {c1_1}\n')
+        print(f'rotate {i} {c1_2}\n')
+        print('OK')
         '''
         line_speed, angle_speed = 3, 1.5
         for robot_id in range(4):
             sys.stdout.write('forward %d %d\n' % (robot_id, line_speed))
-            sys.stdout.write('rotate %d %f\n' % (robot_id, angle_speed))'''
+            sys.stdout.write('rotate %d %f\n' % (robot_id, angle_speed))
+        '''
         finish()
